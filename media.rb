a88007f27088bb01777274df560fa98c19f7418d
BPH <ba.perlaki@gmail.com>
2015-08-13 15:37:08 +0200

    ruby script up

--- media.rb
+++ media.rb
@@ -1,47 +1,6 @@
 require 'json'
+require './device'
 
-class Device
-
-	attr_reader :name, :width, :height
-
-	public
-	def initialize(name, width, height)
-		@name = name
-		@width = width
-		@height = height
-	end
-
-	public
-	def fraction
-		return Rational(width, height).to_s
-	end
-
-	public
-	def rev_fraction
-		return Rational(height, width).to_s
-	end
-
-	public
-	def ratio
-		return (width.to_f / height.to_f).to_s
-	end
-
-	public
-	def rev_ratio
-		return (height.to_f / width.to_f).to_s
-	end
-
-	public
-	def to_portrait
-		return {:name => name, :fraction => fraction, :ratio => ratio}
-	end
-
-	public
-	def to_landscape
-		return {:name => name, :fraction => rev_fraction, :ratio => rev_ratio}
-	end
-
-end
 
 devices = [
 	Device.new("iphone4", 		320, 	480),
@@ -69,8 +28,9 @@ sizes[:portrait] = portraits
 
 puts sizes.to_json
 
+#update the json file
 File.open("media.json", "w") { |file| file.write(sizes.to_json)}
 
-system ("open -a Google\\ Chrome media.json")
+# system ("open -a Google\\ Chrome media.json")
 
 
