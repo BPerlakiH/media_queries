18e506542c0a5b594cc91f93295d305ef5d11bac
BPH <ba.perlaki@gmail.com>
2017-03-11 04:47:44 +0100

    Remove useless public access modifiers

--- device.rb
+++ device.rb
@@ -2,39 +2,32 @@ class Device
 
 	attr_reader :name, :width, :height
 
-	public
 	def initialize(name, width, height)
 		@name = name
 		@width = width
 		@height = height
 	end
 
-	public
 	def fraction
 		return Rational(width, height).to_s
 	end
 
-	public
 	def rev_fraction
 		return Rational(height, width).to_s
 	end
 
-	public
 	def ratio
 		return (width.to_f / height.to_f).to_s
 	end
 
-	public
 	def rev_ratio
 		return (height.to_f / width.to_f).to_s
 	end
 
-	public
 	def to_portrait
 		return {:name => name, :fraction => fraction, :ratio => ratio}
 	end
 
-	public
 	def to_landscape
 		return {:name => name, :fraction => rev_fraction, :ratio => rev_ratio}
 	end
